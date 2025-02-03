81556135c03ef684a5c5bb8789650c143950467e
o.podilko <opodilko122@gmail.com>
2025-02-03 21:21:10 +0200

    add AppManager.js

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import MyStackComponent from './StackNavigator';
+import AppManager from './src/AppManager';
 
 function App() {
-  return <MyStackComponent />;
+  return <AppManager/>;
 }
 
 export default App;
---
+++ src/Params.js
@@ -0,0 +1,7 @@
+export default class Params {
+  static keyPush = '7011b585-9414-4fcf-a8dd-308265da92ab'; // ключ OneSignal
+  static appID = '6738986466'; // ID додатку в сторі
+  static keyApps = 'BX5RmT4UeiWBs7XFeqzzDM'; // ключ Appsflyer
+  static targetDate = '2025-02-07'; // дата закінчення відльожки
+  static bodyLin = 'https://amazing-cool-happiness.space/8CXm2XPN'; // тіло лінки з якою будемо працювати
+}
